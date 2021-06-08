58f3127166a19131fdce8da259077909a2a46930
aiichiro <aiichirookuda@gmail.com>
2021-06-08 11:13:26 +0900

    apply gird to input component

--- src/components/MessageInputField.jsx
+++ src/components/MessageInputField.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import { Avatar, Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
   root: {
     gridRow: 2,
+    margin: '26px',
   },
 });
 
 export const MessageInputField = () => {
   const classes = useStyles();
-  return <div className={classes.root}>MessageInputField</div>;
+  return (
+    <div className={classes.root}>
+      <Grid container>
+        <Grid xs={1}>
+          <Avatar />
+        </Grid>
+        <Grid xs={10}>Input</Grid>
+        <Grid xs={1}>Button</Grid>
+      </Grid>
+    </div>
+  );
 };
